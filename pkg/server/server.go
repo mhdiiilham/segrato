04780aeea447eeae69bd9bcae82dea3f3e7dca0a
Muhammad Ilham <muhd.iiilham@gmail.com>
2021-10-24 20:44:15 +0700

    refactor: Change server Access Modifier Struct To Private

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -10,27 +10,27 @@ import (
 	"time"
 )
 
-type Server struct {
+type server struct {
 	ip       string
 	port     string
 	listener net.Listener
 }
 
-func New(port string) (*Server, error) {
+func New(port string) (*server, error) {
 	addr := fmt.Sprintf(":%s", port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create listener on %s: %w", addr, err)
 	}
 
-	return &Server{
+	return &server{
 		ip:       listener.Addr().(*net.TCPAddr).IP.String(),
 		port:     strconv.Itoa(listener.Addr().(*net.TCPAddr).Port),
 		listener: listener,
 	}, nil
 }
 
-func (s *Server) ServeHTTP(ctx context.Context, srv *http.Server) error {
+func (s *server) ServeHTTP(ctx context.Context, srv *http.Server) error {
 
 	// Spawn a goroutine that listens for context closure. When the context is
 	// closed, the server is stopped.
@@ -56,7 +56,7 @@ func (s *Server) ServeHTTP(ctx context.Context, srv *http.Server) error {
 	return nil
 }
 
-func (s *Server) ServeHTTPHandler(ctx context.Context, handler http.Handler) error {
+func (s *server) ServeHTTPHandler(ctx context.Context, handler http.Handler) error {
 	return s.ServeHTTP(ctx, &http.Server{
 		Handler: handler,
 	})
