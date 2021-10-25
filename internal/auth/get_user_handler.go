66bc0a0554e12bcde7f6613049446682298cd9c0
Muhammad Ilham <muhd.iiilham@gmail.com>
2021-10-25 22:35:27 +0700

    fix: Check Error When Fetch User Data
    Check error when fetch user data. If error is mongo no documents,
    it will return http status code 404

--- internal/auth/get_user_handler.go
+++ internal/auth/get_user_handler.go
@@ -1,15 +1,24 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/mhdiiilham/segrato/internal/auth/model"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func (s *Server) GetUser(c *fiber.Ctx) error {
 	userID := c.Params("userid")
 	user, err := s.userService.GetUser(c.Context(), userID)
 	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(http.StatusNotFound).JSON(model.Error{
+				Code:    http.StatusNotFound,
+				Message: "user not found",
+			})
+		}
 		return c.Status(http.StatusInternalServerError).JSON(struct {
 			Code    int    `json:"code"`
 			Message string `json:"message"`
