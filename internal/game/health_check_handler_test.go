aaf55af364a367d08cdbce24300ebde15e977f92
Muhammad Ilham <muhd.iiilham@gmail.com>
2021-10-28 01:47:31 +0700

    test: Refactor Asserting

--- internal/game/health_check_handler_test.go
+++ internal/game/health_check_handler_test.go
@@ -13,20 +13,24 @@ import (
 )
 
 func Test_server_HealtCheck(t *testing.T) {
-	var resp apiresponse.HealtCheck
 	ctx := context.Background()
+	var resp apiresponse.HealtCheck
+
+	httpCodeExpected := http.StatusOK
+	msgExpected := "game server is running"
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/games/health-check", nil)
+	defer r.Body.Close()
+
 	srv, srvErr := NewServer(config.Config{})
 	assert.NoError(t, srvErr)
 	routes := srv.CORS(srv.HandlerLogging(srv.Routes(ctx)))
 	routes.ServeHTTP(w, r)
 
-	respBody := w.Body.Bytes()
-	unMarshallErr := json.Unmarshal(respBody, &resp)
-	assert.NoError(t, unMarshallErr)
+	assert.NoError(t, json.NewDecoder(w.Body).Decode(&resp), "should be no error when decoding response body")
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, http.StatusOK, resp.Code)
-	assert.Equal(t, "game server is running", resp.Message)
+	assert.Equal(t, http.StatusOK, w.Code, "expecting http code %d, but got %d", httpCodeExpected, w.Code)
+	assert.Equal(t, httpCodeExpected, resp.Code, "expecting http code %d, but got %d", httpCodeExpected, resp.Code)
+	assert.Equal(t, msgExpected, resp.Message, "apiresponse.HealtCheck.Message = %s, want = %s", resp.Message, msgExpected)
 }
